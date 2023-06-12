4abcb7fd5c97111b4bc1e72ed543b62e1ceccfff
codingXpert <vivekraj78965.vr@gmail.com>
2023-06-12 19:30:33 +0530

    error handling

--- controllers/userControlller.js
+++ controllers/userControlller.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel");
-
-const createUser = async (req, res) => {
-  try {
+const asyncHandler = require("express-async-handler");
+const createUser = asyncHandler(async (req, res) => {
+//   try {
     const email = req.body.email;
     console.log(email);
     const findUser = await User.findOne({ email });
@@ -9,12 +9,12 @@ const createUser = async (req, res) => {
       const newUser = await User.create(req.body);
       res.status(201).send(newUser);
     } else {
-      res.json({ message: "User already exist!", success: "false" });
+      throw new Error("User already exist!");
     }
-  } catch (error) {
-    res.send({ message: error.message }); 
-  }
-};
+//   } catch (error) {
+//     res.send({ message: error.message });
+//   }
+});
 module.exports = {
   createUser,
 };
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnection");
+const { notFound , errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 dbConnect();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use('/' , require('./routes/index'))
+app.use('/' , require('./routes/index'));
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT, (err) => {
   if (err) {
     throw err;
