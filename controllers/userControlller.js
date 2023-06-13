5b1f886074a6c665158e915d7ead14cc75d3484e
codingXpert <vivekraj78965.vr@gmail.com>
2023-06-13 19:01:59 +0530

    error correction

--- controllers/userControlller.js
+++ controllers/userControlller.js
@@ -3,20 +3,21 @@ const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
 //user registration
-const createUser = asyncHandler(async (req, res) => {
+const createUser = async (req, res) => {
   try {
     const email = req.body.email;
+    console.log(email);
     const findUser = await User.findOne({ email });
     if (!findUser) {
       const newUser = await User.create(req.body);
       res.status(201).send(newUser);
     } else {
-      throw new Error("User already exist!");
+      res.json({ message: "User already exist!", success: "false" });
     }
   } catch (error) {
     res.send({ message: error.message });
   }
-});
+};
 
 // user login
 const login = async (req, res) => {
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnection");
-const { notFound , errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
@@ -9,8 +8,6 @@ dbConnect();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/' , require('./routes/index'));
-app.use(notFound);
-app.use(errorHandler);
 app.listen(PORT, (err) => {
   if (err) {
     throw err;
